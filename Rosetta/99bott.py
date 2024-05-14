d5881f6274590780837635daf0e226ee56426bf2
EodinV <mattiasvholmgren@gmail.com>
2024-05-14 11:33:13 +0200

    added Rosetta 99bott

---
+++ Rosetta/99bott.py
@@ -0,0 +1,16 @@
+bottles = 99
+
+for i in range(99):
+    print(bottles, " bottles of beer on the wall.")
+    print(" ")
+    print(bottles, " bottles of beer.")
+    print(" ")
+    print("   You take one down,")
+    print(" ")
+    print("   You pass it around.")
+    print(" ")
+    bottles = bottles - 1
+    print(bottles, " bottles of beer on the wall.")
+    print(" ")
+    print(" ")
+    print(" ")
