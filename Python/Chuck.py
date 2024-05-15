dfa208ed06834a4e62191620e82e7ebe5eed81b5
EodinV <mattiasvholmgren@gmail.com>
2024-05-15 10:05:16 +0200

    chuckachucka

--- Python/Chuck.py
+++ Python/Chuck.py
@@ -1,4 +1,5 @@
 import requests
+import turtle
 
 response = requests.get("https://api.chucknorris.io/jokes/random")
 
@@ -6,3 +7,14 @@ prejoke = response.text.split("value")[1]
 joke = prejoke.split(":")[1]
 joke = joke[:-1]
 print(joke)
+
+win = turtle.Screen()
+win.setup(1200, 100)
+win.bgcolor(0.6, 0.6, 0.6)
+win.title("Is a Joke")
+
+turtle.hideturtle()
+turtle.write(joke, move=False, align="center", font=("Stencil", 13, "bold"))
+
+
+turtle.done()
